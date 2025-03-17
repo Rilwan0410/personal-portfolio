8b49588eb0c7296db6c12e6a35ca5353edfe5e86
Rilwan0410 <rilwan410@hotmail.com>
2025-03-16 21:54:36 -0400

    remove typewriter

--- lib/siteData.ts
+++ lib/siteData.ts
@@ -65,7 +65,7 @@ export const aboutData = {
     yearsOfExperience: "3",
     hoursOfWorking: "20",
     projectsDone: "90",
-    typewriter: ["Hi There!👋", "I'm Rilwan", "Web Content Editor", "& Email Developer", "Thank you for stopping by 🫡"],
+    typewriter: ["Hi There!👋", "I'm Rilwan"],
   },
   skills: [
     {
